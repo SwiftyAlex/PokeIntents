528ba659ff968f1e27d238820fb2660b75f2e184
Alex Logan <me@alexanderlogan.co.uk>
2022-11-14 19:32:54 +0000

    Initial Commit

---
+++ PokéIntents/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PokéIntents
+//
+//  Created by Alex Logan on 14/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokéIntents/Poke_IntentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_IntentsApp.swift
+//  PokéIntents
+//
+//  Created by Alex Logan on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Poke_IntentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
